5d591788f1cede396ebe7e9f6af95153fc8bf323
Johannes Unterstein <junterstein@mesosphere.io>
2017-01-06 22:47:02 +0100

    added some black magic to make galera start

--- image/dcos-galera.sh
+++ image/dcos-galera.sh
@@ -2,6 +2,7 @@
 
 # calc public ip
 ip=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
+echo "own ip address: $ip"
 
 # wait 5 seconds for dns to
 echo "waiting 5 seconds for dns"
@@ -9,6 +10,8 @@ sleep 5
 
 # try until DNS is ready
 url="galera.marathon.containerip.dcos.thisdcos.directory"
+members=""
+digs=""
 
 for i in {1..20}
 do
@@ -17,15 +20,25 @@ do
 		echo "no DNS record found for $url"
 	else
 		# calculate discovery members
-		echo $digs
-		members=`echo $digs | sed -e "s/$ip //g" -e 's/ /,/g'`
+		members=`echo $digs | sed -e "s/$ip //g" -e "s/$ip//g" -e 's/ /,/g'`
 		echo "calculated initial discovery members: $members"
 		break
 	fi
    sleep 2
 done
 
-sed -i -e "s/THIS_NODE_IP/$ip/g" -e "s/GCOM_IPS/$members/g" /etc/mysql/conf.d/galera.cnf
+echo "wsrep_node_address=\"$ip\"" >> /etc/mysql/conf.d/galera.cnf
+echo "wsrep_cluster_address=\"gcomm://$members\"" >> /etc/mysql/conf.d/galera.cnf
 
-# start mysqld
-/docker-entrypoint.sh mysqld
+
+# only the first one is allowed to start a new cluster
+if [[ `echo $digs | sed -e "s/\.//g"` == `echo $ip | sed -e "s/\.//g"`* ]]; then
+	echo "starting new cluster"
+	/docker-entrypoint.sh mysqld --wsrep-new-cluster
+else
+	echo "joining existing cluster"
+	# sleep another few seconds
+	sleep 20
+	# start mysqld
+	/docker-entrypoint.sh mysqld
+fi
